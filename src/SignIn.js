a9099497989714949e722ca82883439002d7c57d
ckeefe90 <ckeefe90@yahoo.com>
2020-11-16 20:00:10 -0500

    created sign up and sign in

---
+++ src/SignIn.js
@@ -0,0 +1,42 @@
+import React, { useContext } from 'react';
+import { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import UserContext from './UserContext';
+
+export default function SignIn(props) {
+    const userContext = useContext(UserContext)
+    const history = useHistory()
+    const [error, setError] = useState()
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        const { email, password } = e.target
+        const user = { email: email.value, password: password.value }
+        userContext.setUser(user, () => history.push('/MyPizzaList'))
+            .catch(setError)
+    }
+
+    return (<>
+        <div className='SignIn'>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <h2>Please sign in:</h2>
+                    {error && <>
+                        <h3>Oops! Username and password do not match.</h3>
+                        <h4>{error.message}</h4>
+                    </>}
+                </div>
+                <div>
+                    <label htmlFor='email'>Email:</label>
+                    <input name='email' required />
+                </div>
+                <div>
+                    <label htmlFor='password'>Password:</label>
+                    <input name='password' type='password' required />
+                </div>
+                <button type='submit'>Login</button>
+                <p>Don't have an account? <Link to='/SignUp'>Sign up</Link></p>
+            </form>
+        </div>
+    </>)
+}
---
+++ src/SignUp.js
@@ -0,0 +1,46 @@
+import React, { useContext, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import UserContext from './UserContext';
+
+export default function SignUp() {
+
+    const userContext = useContext(UserContext);
+    const history = useHistory();
+    const [error, setError] = useState();
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        const { email, password, confirmPassword } = e.target
+        if (password.value !== confirmPassword.value) {
+            setError(new Error('Passwords do not match'))
+            return
+        }
+        const user = { email: email.value, password: password.value }
+        userContext.addUser(user, () => history.push('/MyPizzaList'))
+            .catch(setError)
+    }
+
+    return (<>
+        <div className='SignUp'>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <h3>Create an account so you can track your pizzas</h3>
+                </div>
+                <div>
+                    <label htmlFor='email'>Email:</label>
+                    <input name='email' id='email' required />
+                </div>
+                <div>
+                    <label htmlFor='password'>Password:</label>
+                    <input name='password' id='password' type='password' required />
+                </div>
+                <div>
+                    <label htmlFor='confirmPassword'>Confirm Password:</label>
+                    <input name='confirmPassword' id='confirmPassword' type='password' required />
+                </div>
+                <button type='submit'>Sign-up!</button>
+                <p>Already have an account? <Link to='/SignIn'>Sign in</Link></p>
+            </form>
+        </div>
+    </>)
+}
