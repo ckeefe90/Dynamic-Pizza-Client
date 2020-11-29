6b33d3d3cd12a77ed37840855a03a22a1c06e973
ckeefe90 <ckeefe90@yahoo.com>
2020-11-28 23:35:49 -0500

    updated css file

--- src/SignIn.js
+++ src/SignIn.js
@@ -38,9 +38,7 @@ export default function SignIn(props) {
                 <button type='submit'>Login</button>
                 <p>Don't have an account? <Link to='/SignUp'>Sign up</Link></p>
             </form>
-        </div>
-        <div className='SignInImage'>
-            <img width='50%' src={pizzaSlice} alt='pizza slice' />
+            <img src={pizzaSlice} alt='pizza slice' />
         </div>
     </>)
 }
